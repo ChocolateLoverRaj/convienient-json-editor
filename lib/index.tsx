d9c645d04c53d3e49d7c6a738a628bb6c7fe676c
KhajasJS <pararaj25@pewaukeeschools.org>
2021-01-08 19:32:02 +0000

    Next.js Setup

---
+++ lib/index.tsx
@@ -0,0 +1,7 @@
+interface Props {
+  schema: true
+}
+
+const Editor = (props: Props): JSX.Element => <h1>Hi</h1>
+
+export default Editor
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Editor from '../lib/index'
+
+const App = (): JSX.Element => (
+  <>
+    <Editor schema />
+  </>
+)
+
+export default App
