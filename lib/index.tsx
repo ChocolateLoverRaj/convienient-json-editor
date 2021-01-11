4ec688e1cd833777e4fcfb9cbeaea47445a6d602
KhajasJS <pararaj25@pewaukeeschools.org>
2021-01-11 19:05:44 +0000

    Objects and numbers

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,44 @@
-interface Props {
-  schema: true
+export interface NumberSchema {
+  type: 'number'
 }
 
-const Editor = (props: Props): JSX.Element => <h1>Hi</h1>
+export interface ObjectSchema<Props extends string> {
+  type: 'object'
+  properties: {
+    [name: Props]: Schema
+  }
+  required?: Props[]
+}
+
+export type Schema = NumberSchema | ObjectSchema<string>
+
+interface Props {
+  schema: Schema
+}
+
+const Editor = (props: Props): JSX.Element => {
+  switch(props.schema.type) {
+    case 'object':
+      return (
+        <>
+          <table>
+            <tbody>
+              {Object.entries(props.schema.properties).map(([name, schema], index) => (
+                <tr key={index}>
+                  <th>{name}</th>
+                  <td>
+                    <Editor schema={schema} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      )
+    case 'number':
+      return <input type='number' />
+  }
+  
+}
 
 export default Editor
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
-import Editor from '../lib/index'
+import Editor, { Schema } from '../lib/index'
+
+const schema: Schema = {
+  type: 'object',
+  properties: {
+    number: {
+      type: 'number'
+    },
+    object: {
+      type: 'object',
+      properties: {
+        subProp1: {
+          type: 'number'
+        },
+        subProp2: {
+          type: 'number'
+        }
+      }
+    }
+  }
+}
 
 const App = (): JSX.Element => (
   <>
-    <Editor schema />
+    <Editor schema={schema} />
   </>
 )
 
