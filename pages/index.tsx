4f7e2dabe8948711e1463211de242da2bb48990a
KhajasJS <pararaj25@pewaukeeschools.org>
2021-01-11 19:44:07 +0000

    Pretty print json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Editor, { Schema } from '../lib/index'
+import Head from 'next/head'
+import {prettyPrintJson} from 'pretty-print-json'
 
 const schema: Schema = {
   type: 'object',
@@ -20,16 +22,26 @@ const schema: Schema = {
   }
 }
 
-const App = (): JSX.Element => (
-  <>
-    <Editor schema={schema} defaultValue={{
-      number: 100,
-      object: {
-        subProp1: 1,
-        subProp2: 2
-      }
-    }} />
-  </>
-)
+const App = (): JSX.Element => {
+  const toHtml = (prettyPrintJson ?? window.prettyPrintJson).toHtml
+  console.log(toHtml(3))
+  return (
+    <>
+      <Head>
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/pretty-print-json@0.4/dist/pretty-print-json.css' />
+        <script src='https://cdn.jsdelivr.net/npm/pretty-print-json@0.4/dist/pretty-print-json.min.js' />
+      </Head>
+      <Editor schema={schema} defaultValue={{
+        number: 100,
+        object: {
+          subProp1: 1,
+          subProp2: 2
+        }
+      }} />
+      <p>Output Json</p>
+      <div dangerouslySetInnerHTML={{ __html: toHtml(3) }} />
+    </>
+  )
+}
 
 export default App
