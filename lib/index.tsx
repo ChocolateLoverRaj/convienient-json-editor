d36ca3afa20518a1c3f58ff09a5d4b19fe775656
KhajasJS <pararaj25@pewaukeeschools.org>
2021-01-12 13:23:46 +0000

    onChange

--- lib/index.tsx
+++ lib/index.tsx
@@ -20,27 +20,46 @@ interface Props {
   readOnly?: boolean
 }
 
+interface NumberProps extends Props {
+  schema: NumberSchema
+}
+const NumberEditor = (props: NumberProps): JSX.Element => (
+  <input type='number' readOnly={props.readOnly} defaultValue={props.defaultValue} value={props.value} onChange={e => {
+    props.onChange(parseInt(e.target.value))
+  }} />
+)
+
+interface ObjectProps extends Props {
+  schema: ObjectSchema
+}
+const ObjectEditor = (props: ObjectProps): JSX.Element => (
+  <>
+    <table>
+      <tbody>
+        {Object.entries(props.schema.properties).map(([name, schema], index) => (
+          <tr key={index}>
+            <th>{name}</th>
+            <td>
+              <Editor schema={schema} readOnly={props.readOnly} defaultValue={props.defaultValue?.[name]} value={props.value?.[name]} onChange={value => {
+                props.onChange({
+                  ...props.value,
+                  [name]: value
+                })
+              }} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </>
+)
+
 const Editor = (props: Props): JSX.Element => {
   switch(props.schema.type) {
     case 'object':
-      return (
-        <>
-          <table>
-            <tbody>
-              {Object.entries(props.schema.properties).map(([name, schema], index) => (
-                <tr key={index}>
-                  <th>{name}</th>
-                  <td>
-                    <Editor schema={schema} readOnly={props.readOnly} defaultValue={props.defaultValue?.[name]} />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
-      )
+      return <ObjectEditor {...props} />
     case 'number':
-      return <input type='number' readOnly={props.readOnly} defaultValue={props.defaultValue} />
+      return <NumberEditor {...props} />
   }
   
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Editor, { Schema } from '../lib/index'
 import Head from 'next/head'
-import {prettyPrintJson} from 'pretty-print-json'
+import { prettyPrintJson } from 'pretty-print-json'
+import { useState } from 'react'
 
 const schema: Schema = {
   type: 'object',
@@ -23,23 +24,23 @@ const schema: Schema = {
 }
 
 const App = (): JSX.Element => {
+  const [json, setJson] = useState({
+    number: 100,
+    object: {
+      subProp1: 1,
+      subProp2: 2
+    }
+  })
   const toHtml = (prettyPrintJson ?? window.prettyPrintJson).toHtml
-  console.log(toHtml(3))
   return (
     <>
       <Head>
         <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/pretty-print-json@0.4/dist/pretty-print-json.css' />
         <script src='https://cdn.jsdelivr.net/npm/pretty-print-json@0.4/dist/pretty-print-json.min.js' />
       </Head>
-      <Editor schema={schema} defaultValue={{
-        number: 100,
-        object: {
-          subProp1: 1,
-          subProp2: 2
-        }
-      }} />
+      <Editor schema={schema} value={json} onChange={setJson} />
       <p>Output Json</p>
-      <div dangerouslySetInnerHTML={{ __html: toHtml(3) }} />
+      <pre dangerouslySetInnerHTML={{ __html: toHtml(json) }} />
     </>
   )
 }
